5a54ef26c74e52a97a88108f46d96c81aaf0f2d8
Fodenga <mail2.so.a@gmail.com>
2023-06-06 18:57:58 +0100

    Button component + Initial Landing page

---
+++ src/components/Button.js
@@ -0,0 +1,47 @@
+import Link from "next/link"
+import clsx from "clsx"
+
+function Button({children, className, onClick, disabled}) {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+            className={clsx("rounded-md bg-cyan-600 py-2 px-3 text-sm font-semibold text-white shadow-sm",
+                "hover:bg-cyan-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+                "focus-visible:outline-cyan-600 disabled:opacity-50 disabled:bg-cyan-500 disabled:cursor-not-allowed", className)}>
+            {children}
+        </button>
+    )
+}
+
+function SecondaryButton({children, className, onClick}) {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className={clsx("rounded-md bg-white py-2 px-3 text-sm font-semibold text-gray-900 shadow-sm",
+                "ring-1 ring-inset ring-gray-300 hover:bg-gray-50", className)}>
+            {children}
+        </button>
+    )
+}
+
+function ButtonLink({to, children, className, disabled}) {
+    return (
+        <Link
+            href={to}
+            disabled={disabled}
+            className={clsx("flex justify-center rounded-md bg-cyan-600 py-2 px-3 text-sm font-semibold text-white shadow-sm",
+                "hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+                "focus-visible:outline-cyan-600 disabled:opacity-50 disabled:bg-cyan-500 disabled:cursor-not-allowed", className)}>
+            {children}
+        </Link>
+    )
+}
+
+export {
+    Button,
+    ButtonLink,
+    SecondaryButton
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,36 @@
 import Image from 'next/image'
-
+import {Button,ButtonLink} from '../components/Button'
 export default function Home() {
     return (
-        <div>
+        <div className="mx-auto max-w-2xl justify-center">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-slate-300 sm:text-6xl">
+                        Welcome to <span className='text-cyan-600'>Basket</span>
+                </h1>
+            </div>
+
+            <div className="text-center">
+                <h1 className="mt-20 text-2xl font-bold tracking-tight text-slate-400 sm:text-4xl">Your gateway to crypto diversification!</h1>
+            </div>
+
+            <p className="mt-10 text-lg leading-8 text-slate-400">Our platform allows you to effortlessly invest in multiple cryptocurrencies with just one click. Harnessing the power of blockchain technology, we ensure transparency, reduce transaction costs, and automate portfolio rebalancing. With Basket, investing in the dynamic world of cryptocurrencies becomes easy, efficient, and secure. Welcome to the future of crypto investment.</p>
+
+
+            <div className="flex mt-10 justify-center gap-8">
+                <ButtonLink to="/investing" className="w-72">
+                    Get started
+                </ButtonLink>
+                {/*<p className="text-slate-400">or</p>*/}
+                <ButtonLink to='/community' className="w-72">
+                    Track your Transactions!
+                </ButtonLink>
+                    </div>
 
         </div>
+            
+            
+            
+            
+
     )
 }
