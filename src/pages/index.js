9f8f46264024416e530eaf5a8a1aaa857f254057
Fodenga <mail2.so.a@gmail.com>
2023-06-06 19:57:09 +0100

    Responsiveness on Buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import {Button,ButtonLink} from '../components/Button'
+import {BookOpenIcon} from '@heroicons/react/24/outline'
+
 export default function Home() {
     return (
         <div className="mx-auto max-w-2xl justify-center">
@@ -13,18 +15,19 @@ export default function Home() {
                 <h1 className="mt-20 text-2xl font-bold tracking-tight text-slate-400 sm:text-4xl">Your gateway to crypto diversification!</h1>
             </div>
 
-            <p className="mt-10 text-lg leading-8 text-slate-400">Our platform allows you to effortlessly invest in multiple cryptocurrencies with just one click. Harnessing the power of blockchain technology, we ensure transparency, reduce transaction costs, and automate portfolio rebalancing. With Basket, investing in the dynamic world of cryptocurrencies becomes easy, efficient, and secure. Welcome to the future of crypto investment.</p>
+            <p className="mt-10 text-lg leading-8 text-slate-400">Our platform allows you to effortlessly invest in multiple cryptocurrencies with just one click. Harnessing the power of blockchain technology, we ensure transparency, reduce transaction costs, and automate portfolio rebalancing. With Basket, investing in the dynamic world of cryptocurrencies becomes easy, efficient, and secure. Welcome to the future of crypto investment!</p>
 
 
-            <div className="flex mt-10 justify-center gap-8">
-                <ButtonLink to="/investing" className="w-72">
+            <div className="flex flex-col mt-10 justify-center gap-8 sm:flex-row gap-4 items-center">
+                <ButtonLink to="/investing" className="w-72 items-center">
                     Get started
                 </ButtonLink>
                 {/*<p className="text-slate-400">or</p>*/}
-                <ButtonLink to='/community' className="w-72">
-                    Track your Transactions!
+                <ButtonLink to='/community' className="w-72 bg-slate-600 items-center hover:bg-slate-500">
+                     <BookOpenIcon className="h-8 w-8 mx-2"/>
+                     Learn More!
                 </ButtonLink>
-                    </div>
+            </div>
 
         </div>
             
