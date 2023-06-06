e4a2eea24807338b62e759c2f789fa640aba6392
aschenkuttel <aschenkuttel@gmail.com>
2023-06-06 15:38:15 +0100

    supabase connection

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,4 +1,5 @@
 import {createContext, Component} from "react"
+import Supabase from "@/lib/supabase"
 
 
 const BasketContext = createContext({})
@@ -6,6 +7,8 @@ const BasketContext = createContext({})
 export default class BasketProvider extends Component {
     constructor(props) {
         super(props);
+
+        this.db = new Supabase()
     }
 
     render() {
