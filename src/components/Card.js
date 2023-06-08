2564a2a2dcf8b09e5ca6569d4ad0515d57c06d28
aschenkuttel <aschenkuttel@gmail.com>
2023-06-08 17:41:36 +0100

    why do I have to be sick

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 export default function Card({title, children}) {
     return (
-        <div className="max-w-md w-72 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+        <div className="flex flex-col max-w-md w-72 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
             <div className="px-4 py-5 text-center text-2xl text-cyan-600 sm:px-6">
                 {title}
             </div>
-            <div className="px-4 py-5 flex flex-col sm:p-6">
+            <div className="flex-1 px-4 py-5 flex flex-col sm:p-6">
                 {children}
             </div>
         </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,8 @@
 import Card from "@/components/Card"
 import {BasketContext} from "@/components/Context"
 import {useContext} from "react"
-import { ButtonLink } from "./Button"
+import {ButtonLink} from "./Button"
+import Image from "next/image"
 
 
 export default function Display({children}) {
@@ -12,31 +13,38 @@ export default function Display({children}) {
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
             {baskets.map((basket, index) => (
                 <Card key={`card_${index}`} title={basket.title}>
-                            <p>{basket.description}</p>
-                            <p className="mt-4 text-center text-yellow-500">{basket.collected}$/{basket.target}$</p>
-                    
-                        <div className="flex flex-col justify-center gap-y-5">
-                            <div className="flex mt-4  -space-x-0.5 gap-1">
-                                <dt className="sr-only">Assets</dt>
-                                    <p>Assets: </p>
-                                    {assets.map((asset, index) => (
-                                        <dd className = 'flex flex-row' key={asset.address}>
-                                            <img
-                                                className="h-5 w-5 rounded-full bg-gray-50 ring-2 ring-white"
-                                                src={asset.iconUrl}
-                                                alt={asset.name}
-                                            />
-                                        </dd>
-                                ))}
-                                
-                            </div>
+                    <p className="flex-1">{basket.description}</p>
 
-                            <ButtonLink to={`/basket/${basket.id}`}>
-                            See details!
-                            </ButtonLink>
+                    <div className="mt-4">
+                        <p className="font-medium text-gray-700">
+                            Owned / Total:
+                        </p>
+                        <p className="text-yellow-500">{basket.collected}$/{basket.target}$</p>
+                    </div>
+
+                    <div className="flex flex-col justify-center gap-y-5">
+                        <div className="flex flex-col mt-4 -space-x-0.5 gap-1">
+                            <dt className="sr-only">Assets</dt>
+                            <p className="font-medium text-gray-700">
+                                Assets:
+                            </p>
+                            <div className="flex gap-1">
+                            {assets.map((asset, index) => (
+                                <Image key={asset.id} width={24} height={24}
+                                       className="h-5 w-5 rounded-full bg-gray-50 ring-2 ring-white"
+                                       src={asset.iconUrl}
+                                       alt={asset.name}
+                                />
+                            ))}
+                            </div>
                         </div>
-                        
-                                           
+
+                        <ButtonLink to={`/basket/${basket.id}`}>
+                            See details!
+                        </ButtonLink>
+                    </div>
+
+
                 </Card>
             ))}
         </div>
